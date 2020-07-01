5decdb7594c50f04f328f1761314f0175c190076
MarKoRn <mark7dev@hotmail.com>
2020-07-01 01:03:41 -0500

    setup redux

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import Header from './components/Header';
 import Form from './components/Form';
 import Table from './components/Table';
 import Student from './components/Student';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
     <Router>
+      <Provider store={store}>
         <Header />
         <Switch>
           <Route exact path="/" component={Form} />
           <Route exact path="/table" component={Table} />
           <Route exact path="/student/:id" component={Student} />
         </Switch>
+      </Provider>
     </Router>
   );
 }
